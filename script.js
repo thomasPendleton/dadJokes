16140451c6ff76d6226f63c4364bf0ec2ed0e232
Thomas <thomas6576@yahoo.com>
2022-01-27 12:47:07 -0800

    finished project

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-const jokeBtn = document.querySelector('.get-joke')
+const jokeBtn = document.getElementById('jokeBtn')
+const jokeEl = document.getElementById('joke')
 
+// With promises
+// generateJoke()
+// function generateJoke(){
+//     const config = {
+//         headers: {
+//             'Accept': "application/json"
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com/', config)
+//     .then(response => response.json() )
+//     .then(data => {
+//         jokeEl.innerHTML = data.joke
+//      })
+// }
+
+//With Async Await
+getJoke()
 async function getJoke() {
-    const result = await fetch('https://icanhazdadjoke.com/', {method: "GET"})
-    console.log(result.json());
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+  const result = await fetch('https://icanhazdadjoke.com/', config)
+  const data = await result.json()
+  jokeEl.innerHTML = data.joke
 }
 
-jokeBtn.addEventListener('click', ()=> {
-    //Function to get joke from dad joke api
-    getJoke()
+jokeBtn.addEventListener('click', () => {
+  //Function to get joke from dad joke api
+  //promise function
+  //   generateJoke()
+  // async function call
+  getJoke()
 })
